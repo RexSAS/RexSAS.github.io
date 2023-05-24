f02a3975b5152541ae1dbf5097d5fc029e5ef947
RexSAS <114292543+RexSAS@users.noreply.github.com>
2023-05-24 10:49:28 +0200

    Update script.cpp

--- downloadable/script.cpp
+++ downloadable/script.cpp
@@ -3,27 +3,40 @@
 using namespace std;
 
 class Phone{
-   private:
+    private:
         float mAh, V;
-        
+    public:
+        float mAh_value(){
+            return mAh;
+        }
+        float V_value(){
+            return V;
+        }
+        void set_mAh(){
+            cout << "Inserisci i mAh: ";
+            cin >> mAh;
+        }
+        void set_V(){
+            cout << "Inserisci i V: ";
+            cin >> V;
+        }
 };
 
 int main() {
-    float mAh, V;
-    cout << "Inserisci i mAh: ";
-    cin >> mAh;
-    cout << "Inserisci i V: ";
-    cin >> V;
+    Phone phone;
+    
+    phone.set_mAh();
+    phone.set_V();
+    
+    float Wh = phone.mAh_value() * phone.V_value() / 1000.0;
+    float KWh = Wh / 1000.0;
+    float co2 = KWh * 0.4;
 
-    double Wh = mAh * V / 1000;
-    double KWh = Wh / 1000;
-    double co2 = KWh * 0.4;
-
-    ofstream f("output.txt");
-    if (f.is_open()) {
-        f << co2 << " Kg";
-        f.close();
-        cout << "Dati scritti correttamente su output.txt." << endl;
+    ofstream outputFile("output.txt");
+    if (outputFile.is_open()) {
+        outputFile << "Your phone stats:" << "\nmAh: " << phone.mAh_value() << "\nV: " << phone.V_value() << "\nCO2 producted: "<<co2<<" Kg"<<" \n\n\nCoded by Andrea Faveri 4S 2022-2023 Liceo L. Ariosto Ferrara";
+        outputFile.close();
+        cout << "Dati scritti su output.txt." << endl;
     } else {
         cout << "Impossibile aprire il file di output." << endl;
     }
